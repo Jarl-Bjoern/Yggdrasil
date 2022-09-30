613d5f1e95d8f592f40372caafa3989618ae3a31
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2022-09-30 21:40:37 +0200

    Update minimal_install.sh

--- minimal_install.sh
+++ minimal_install.sh
@@ -97,7 +97,7 @@ if __name__ == '__main__':
         for i in range(1, len(Array_Container)):
                 system(f'docker pull {Array_Container[i].split(" ")[0]};')
 EOF
-chmod 600 /home/$USER/.updater
+chmod 600 /home/$USER/.updater/image_updater.py
 cat <<EOF >> /etc/crontab
 0 6     * * *  root /usr/bin/env python3 /home/$USER/.updater/image_updater.py
 EOF
