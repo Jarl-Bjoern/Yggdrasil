53341d7dcac4a32268d7170b761b3f108c1b2526
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2022-10-01 15:30:57 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -76,9 +76,9 @@ clear ; initials
 echo -e "\n          Please choose between a installation"
 echo "----------------------------------------------------------"
 echo "|                                                        |"
-echo -e "|          ${GREEN}full${NOCOLOR}    : full installation (GUI)             |"
-echo -e "|          ${ORANGE}minimal${NOCOLOR} : minimal installation (CLI)          |"
-echo -e "|          ${RED}special${NOCOLOR} : special installation                |"
+echo -e "|            ${GREEN}full${NOCOLOR}    : full installation (GUI)           |"
+echo -e "|            ${ORANGE}minimal${NOCOLOR} : minimal installation (CLI)        |"
+echo -e "|            ${RED}special${NOCOLOR} : special installation              |"
 echo "|                                                        |"
 echo -e "----------------------------------------------------------\n"
 
