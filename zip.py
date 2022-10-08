80def7bc51a79f9434aabd9bf3a1e18d19e6fc5b
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2022-10-08 11:17:48 +0200

    Update zip.py

--- zip.py
+++ zip.py
@@ -10,7 +10,5 @@ from sys import argv
 
 # Main
 if __name__ == '__main__':
-        try:
-                unpack_archive(f'/opt/{argv[1]}', f'/opt')
-                remove (f'/opt/{argv[1]}')
-        except: pass
+    unpack_archive(f'/opt/{argv[1][:-1]}', f'/opt')
+    remove (f'/opt/{argv[1][:-1]}')
