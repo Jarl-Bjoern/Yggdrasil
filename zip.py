0806e0d483d48d8726af09abf2c04ba3538988f4
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2022-10-22 16:57:56 +0200

    Update zip.py

--- zip.py
+++ zip.py
@@ -6,12 +6,17 @@
 
 # Libraries
 from os import remove
+from re import finditer
 from shutil import unpack_archive
 from sys import argv
 
 # Main
 if __name__ == '__main__':
-    if ('\n' in argv[1]): File = argv[1][:-1]
-    else: File = argv[1]
+    if ("/" in argv[1]):
+        for Slash in finditer("/", argv[1]): Position = Slash.span()[1]
+        File = argv[1][Position:]
+    else:
+        if ('\n' in argv[1]): File = argv[1][:-1]
+        else: File = argv[1]
     unpack_archive(f'/opt/pentest_tools/{File}', f'/opt/pentest_tools/')
     remove (f'/opt/pentest_tools/{File}')
