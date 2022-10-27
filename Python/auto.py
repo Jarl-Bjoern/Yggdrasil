6fd472022961d9fb74a2668d18219e5d55854ba2
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2022-10-27 19:56:19 +0200

    Update auto.py

--- Python/auto.py
+++ Python/auto.py
@@ -3,5 +3,11 @@
 # Rainer Christian Bjoern Herold
 # Version 0.1 07.10.2022
 
+# Libraries
+from pyautogui import hotkey, press, write as autowrite
+from time import sleep
+from threading import Thread
+
 # Main
-if __name__ == '__main__': pass
+if __name__ == '__main__':
+  pass
