06ba42a6cc7dad003e3c4b6ca433a9f486dfe9f0
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2022-10-29 13:11:32 +0200

    Update clean.py

--- Python/clean.py
+++ Python/clean.py
@@ -1,1 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
+# Libraries
+from os import remove, walk
+from os.path import exists, join
+from sys import argv
+
+# Arrays
+Array_Filter = ['.sh','.deb','.tar','.bz2','.zip','.rar']
+
+# Main
+if __name__ == '__main__':
+  for root, _, files in walk(str(argv[1]), topdown=False):
+    for file in files:
+      for Filter in Array_Filter:
+        if (file.endswith(Filter)): remove(join(argv[1], file))
