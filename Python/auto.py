2d9bc0e7bdd270c0c74218da3d2a4fbf907005f6
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2022-10-29 11:31:19 +0200

    Update auto.py

--- Python/auto.py
+++ Python/auto.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # Rainer Christian Bjoern Herold
-# Version 0.1 07.10.2022
+# Version 0.1 29.10.2022
 
 # Libraries
 from pyautogui import hotkey, press, write as autowrite
+from sys import argv
 from time import sleep
 from threading import Thread
 
