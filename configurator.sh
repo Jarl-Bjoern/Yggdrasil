fb66f5435fd092433659ca0e4739ad0f16667e7e
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2022-11-02 17:19:06 +0100

    Update configurator.sh

--- configurator.sh
+++ configurator.sh
@@ -333,6 +333,57 @@ hardstatus string "%{.bW}%-w%{.rW}%n %t%{-}%+w %=%{..G} %Y-%m-%d %c "
 EOF
 done
 
+# Vim_Configuration (Thx to @HomeSen)
+for i in $(ls /home | grep -v "lost+found") $(echo /root); do
+        if [[ !($i = "/root") ]]; then
+                PATH_VIM="/home/$i/.vimrc"
+        else
+                PATH_VIM="/root/.vimrc"
+        fi
+	cat <<EOF > $PATH_VIM
+syntax on
+
+" Uncomment the following to have Vim jump to the last position when
+" reopening a file
+au BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif
+
+" Uncomment the following to have Vim load indentation rules and plugins
+" according to the detected filetype.
+filetype plugin indent on
+
+"set showcmd            " Show (partial) command in status line.
+set showmatch           " Show matching brackets.
+set ignorecase          " Do case insensitive matching
+"set smartcase          " Do smart case matching
+"set incsearch          " Incremental search
+"set autowrite          " Automatically save before commands like :next and :make
+"set hidden             " Hide buffers when they are abandoned
+set mouse=              " Enable mouse usage (all modes)
+
+set number
+set background=dark
+
+" Custom status line
+set laststatus=2
+set statusline=%F
+set statusline+=\ 
+set statusline+=%r
+set statusline+=\ 
+set statusline+=%y
+set statusline+=%=
+set statusline+=pos:
+set statusline+=\ 
+set statusline+=%c
+set statusline+=\ 
+set statusline+=[\ 
+set statusline+=%l
+set statusline+=\ /\ 
+set statusline+=%L
+set statusline+=\ ]
+set statusline+=\ 
+set statusline+=%p%%
+EOF
+
 if [[ $category_type = "pentest" || $category_type = "4" || $category_type = "complete" || $category_type = "1" ]];  then
 	# Git_Tools_Installation
 	if [ -f "/opt/pentest_tools/PEASS-ng/metasploit/peass.rb" ]; then
