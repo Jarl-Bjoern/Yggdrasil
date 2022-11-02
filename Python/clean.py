b738c387bc52fee2f4e6cb777d2330cab57d8e02
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2022-11-02 18:45:01 +0100

    Update clean.py

--- Python/clean.py
+++ Python/clean.py
@@ -14,4 +14,6 @@ if __name__ == '__main__':
   for root, _, files in walk(str(argv[1]), topdown=False):
     for file in files:
       for Filter in Array_Filter:
-        if (file.endswith(Filter)): remove(join(argv[1], file))
+        try:
+            if (file.endswith(Filter)): remove(join(argv[1], file))
+        except FileNotFoundError: pass
