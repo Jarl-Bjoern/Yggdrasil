a64e691fadd210c42cc4eda0e9a77fa6f7d811e5
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2022-11-18 19:02:23 +0100

    Update configurator.sh

--- configurator.sh
+++ configurator.sh
@@ -94,7 +94,7 @@ function header() {
 		echo -e "${CYAN}|${NOCOLOR} [${RED}1${NOCOLOR}] ${RED}complete${NOCOLOR}    :   complete configuration      ${CYAN}|${NOCOLOR}"
 		echo -e "${CYAN}|${NOCOLOR} [${CYAN}2${NOCOLOR}] ${CYAN}Firewall${NOCOLOR}      :   firewall hardening       ${CYAN}|${NOCOLOR}"
 		echo -e "${CYAN}|${NOCOLOR} [${GREEN}3${NOCOLOR}] ${GREEN}Sysctl (OS)${NOCOLOR}    :   sysctl hardening     ${CYAN}|${NOCOLOR}"
-		echo -e "${CYAN}|${NOCOLOR} [${ORANGE}4${NOCOLOR}] ${ORANGE}SSH${NOCOLOR}     :   SSH hardening      ${CYAN}|${NOCOLOR}"		
+		echo -e "${CYAN}|${NOCOLOR} [${ORANGE}4${NOCOLOR}] ${ORANGE}SSH${NOCOLOR}     :   SSH hardening      ${CYAN}|${NOCOLOR}"
 	fi
 	echo -e "${CYAN}|${NOCOLOR}                                                        ${CYAN}|${NOCOLOR}"
 	echo -e "${CYAN}----------------------------------------------------------${NOCOLOR}\n"
