142122f7a53191c6f6f3ca9ca80dfe0de8f7200b
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2022-11-21 20:20:13 +0100

    Update auto.py

--- Python/auto.py
+++ Python/auto.py
@@ -28,7 +28,7 @@ def Veracrypt_Install(Path):
 
   def Auto_Install():
       Press_Key('1', 2), Press_Key('return', 2), Press_Key('return', 2)
-      for i in range(0,20): Press_Hotkey('ctrl','v',1)
+      for i in range(0,20): Press_Hotkey('ctrl', 'v', 1)
       for i in range(0,30): Press_Key('backspace', 0.5)
       autowrite("yes"), sleep(1)
       Press_Key('return', 2), press("return")
