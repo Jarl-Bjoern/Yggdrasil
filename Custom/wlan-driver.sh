ceca043e0dd62c30244b8d83e97fe6e581fb1af2
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2022-12-07 17:51:03 +0100

    Update wlan-driver.sh

--- Custom/wlan-driver.sh
+++ Custom/wlan-driver.sh
@@ -1,5 +1,5 @@
 # TP-Link 821n V6
-sudo apt install -y bc firmware-realtek realtek-rtl8188eus-dkms linux-headers-generic build-essential dkms
+sudo apt install -y bc build-essential dkms firmware-realtek linux-headers-generic realtek-rtl8188eus-dkms 
 sudo mkdir -p /opt/drivers ; cd /opt/drivers ; sudo git clone https://www.github.com/Mange/rtl8192eu-linux-driver ; cd rtl8192eu-linux-driver
 sudo make &&
 sudo make install &&
