a78bf211a38b595d136d9dc7b7260d6349580eb4
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2022-12-07 20:42:39 +0100

    Update configurator.sh

--- configurator.sh
+++ configurator.sh
@@ -446,9 +446,7 @@ if [[ $Switch_Skip != true ]]; then
                 done
         else
                 if [[ $hardening = "complete" || $hardening = "1" ]]; then
-                        Switch_Firewall=true
-                        Switch_Hardening=true
-                        Switch_SSH=true
+                        Switch_Firewall=true ; Switch_Hardening=true ; Switch_SSH=true
                 elif [[ $hardening = "firewall" || $hardening = "2" ]];  then
                         Switch_Firewall=true
                 elif [[ $hardening = "sysctl" || $hardening = "3" ]];  then
