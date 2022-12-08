575fbae49aabf91b8e657bbe0fb5f37c9d0700e7
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2022-12-08 19:20:51 +0100

    Update configurator.sh

--- configurator.sh
+++ configurator.sh
@@ -620,10 +620,11 @@ done
 
 if [[ $category_type = "pentest" || $category_type = "4" || $category_type = "complete" || $category_type = "1" ]];  then
 	# Apache_Configuration (UNDER CONSTRUCTION)
-	sudo a2enmod ssl ; sudo sed -i "s/Listen 80/#Listen 80/g" /etc/apache2/ports.conf ; sudo rm -f /var/www/html/index.html
-	sudo mkdir -p /etc/apache2/ssl ; sudo openssl req -x509 -newkey rsa:4096 -keyout /etc/apache2/ssl/pentest-key.pem -out /etc/apache2/ssl/pentest-cert.pem -sha512 -days 365 -asubj '/CN=pentest-kali'
-	if [[ ! $(cat /etc/apache2/apache2.conf | grep -E "ServerSignature Off|ServerTokens Prod")]]; then
-		cat <<EOF >> /etc/apache2/apache2.conf
+	if [[ $(apt-cache policy apache2 | grep "Installed" | cut -d ":" -f2) != "(none)" ]]; then
+		sudo a2enmod ssl ; sudo sed -i "s/Listen 80/#Listen 80/g" /etc/apache2/ports.conf ; sudo rm -f /var/www/html/index.html
+		sudo mkdir -p /etc/apache2/ssl ; sudo openssl req -x509 -newkey rsa:4096 -keyout /etc/apache2/ssl/pentest-key.pem -out /etc/apache2/ssl/pentest-cert.pem -sha512 -days 365 -asubj '/CN=pentest-kali'
+		if [[ ! $(cat /etc/apache2/apache2.conf | grep -E "ServerSignature Off|ServerTokens Prod")]]; then
+			cat <<EOF >> /etc/apache2/apache2.conf
 ServerSignature Off
 ServerTokens Prod
 EOF
@@ -642,6 +643,7 @@ EOF
 #
 #<VirtualHost>
 #EOF
+	fi
 
 	# Git_Tools_Installation
 	if [ -d "/opt/pentest_tools/chisel" ]; then
