bd86e7eb384348e5b0251925b4aac0747be50914
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-01-11 19:11:40 +0100

    Update Libraries.py

--- Python/Resources/Libraries.py
+++ Python/Resources/Libraries.py
@@ -1,1 +1,11 @@
-
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Rainer Christian Bjoern Herold
+try:
+    from argparse import ArgumentParser, FileType, RawTextHelpFormatter, SUPPRESS
+    from os import makedirs, name as osname, system, walk
+    from os.path import dirname, join, realpath
+    from subprocess import DEVNULL, getoutput, run
+    from sys import stdout
+    from time import sleep
+except ModuleNotFoundError as e: input(f"The module was not found\n\n{e}\n\nPlease confirm with the button 'Return'"), exit()
