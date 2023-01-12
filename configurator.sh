0817db124e911986053769d11d76d87030c33a18
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-01-12 14:58:23 +0100

    Update configurator.sh

--- configurator.sh
+++ configurator.sh
@@ -815,20 +815,16 @@ EOF
 			fi
 		done
 		for git_wordlist in $(ls /opt/wordlists | grep -v "kali_wordlists"); do
-			if [[ -d $git_wordlist ]]; then
-				if [[ ! $(cat $OPT_Path/update.info | grep $git_wordlist) ]]; then
-					find "/opt/wordlists" -name $git_wordlist | head -n 1 >> $OPT_Path/update.info
-				fi
+			if [[ ! $(cat $OPT_Path/update.info | grep $git_wordlist) ]]; then
+				find "/opt/wordlists" -name $git_wordlist | head -n 1 >> $OPT_Path/update.info
 			fi
 		done
 	else
 		for git_tool in ${Array_GIT_Updater[@]}; do
 			find $OPT_Path -name $git_tool | head -n 1 >> $OPT_Path/update.info
 		done
-		for git_wordlist in $(ls /opt/wordlists | grep -v "kali_wordlists"); do
-			if [[ -d $git_wordlist ]]; then
-				find "/opt/wordlists" -name $git_wordlist | head -n 1 >> $OPT_Path/update.info
-			fi
+		for git_wordlist in $(ls /opt/wordlists | grep -v "kali_wordlists|*.txt"); do
+			find "/opt/wordlists" -name $git_wordlist | head -n 1 >> $OPT_Path/update.info
 		done
 	fi
 fi
