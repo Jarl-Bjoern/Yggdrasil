8862ed0dc1c3f1f1336d544a4d9f2b13142c9231
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-01-18 19:22:25 +0100

    Update nic.py

--- Python/nic.py
+++ Python/nic.py
@@ -6,12 +6,15 @@
 from subprocess import getoutput
 from Resources.Colors import Colors
 
-NIC = getoutput('ip --brief a | grep "UP" | grep -v "lo|docker|veth"').split(' ')
-for IP_Address in range(0, len(NIC)):
-        if (IP_Address == len(NIC)-1): print(Colors.CYAN+'-----------------------------------------------------------------'+Colors.RESET)
-        if (NIC[IP_Address] != "" and NIC[IP_Address] != "UP"):
-                if ("/" in NIC[IP_Address]):
-                        if (NIC[IP_Address].count(':') > 1): print (f'  - {NIC[IP_Address].split("/")[0]} '+Colors.CYAN+'(IPv6)'+Colors.RESET)
-                        else: print(f'  - {NIC[IP_Address].split("/")[0]} '+Colors.GREEN+'(IPv4)'+Colors.RESET)
-                else:
-                        print (Colors.ORANGE+f'{NIC[IP_Address]}:'+Colors.RESET)
+Array_NIC = getoutput('ip --brief a | grep "UP" | grep -v "lo|docker|veth"').splitlines()
+
+for Network_Adapter in range(0, Array_NIC):
+        NIC = Array_NIC[Network_Adapter].split()
+        if (IP_Address == len(Array_NIC)-1): print(Colors.CYAN+'-----------------------------------------------------------------'+Colors.RESET)
+        for IP_Address in range(0, len(NIC)):
+                if (NIC[IP_Address] != "" and NIC[IP_Address] != "UP"):
+                        if ("/" in NIC[IP_Address]):
+                                if (NIC[IP_Address].count(':') > 1): print (f'  - {NIC[IP_Address].split("/")[0]} '+Colors.CYAN+'(IPv6)'+Colors.RESET)
+                                else: print(f'  - {NIC[IP_Address].split("/")[0]} '+Colors.GREEN+'(IPv4)'+Colors.RESET)
+                        else:
+                                print (Colors.ORANGE+f'{NIC[IP_Address]}:'+Colors.RESET)
