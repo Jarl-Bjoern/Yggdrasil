cb73e8d849ad28507d42de193755d8e59db06478
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-02-02 19:22:38 +0100

    Update configurator.sh

--- configurator.sh
+++ configurator.sh
@@ -674,36 +674,36 @@ if [[ $category_type = "pentest" || $category_type = "4" || $category_type = "co
 
 	# Categories_Sort
 	cd /opt/pentest_tools
-	if [[ $(ls /opt/pentest_tools/{"nmap-erpscan","pysap","PyRFC","SAP_GW_RCE_exploit","SAP_RECON"} ]]; then
+	if [[ $(ls /opt/pentest_tools/{"nmap-erpscan","pysap","PyRFC","SAP_GW_RCE_exploit","SAP_RECON"}) ]]; then
 		sudo mkdir -p /opt/pentest_tools/Webscanner/SAP
 		mv nmap-erpscan pysap PyRFC SAP_GW_RCE_exploit SAP_RECON /opt/pentest_tools/Webscanner/SAP || sudo rm -rf nmap-erpscan pysap PyRFC SAP_GW_RCE_exploit SAP_RECON
 	fi
-	if [[ $(ls /opt/pentest_tools/{"drupwn","droopescan","CMSmap","ac-drupal"} ]]; then
+	if [[ $(ls /opt/pentest_tools/{"drupwn","droopescan","CMSmap","ac-drupal"}) ]]; then
 		sudo mkdir -p /opt/pentest_tools/Webscanner/Drupal
 		mv drupwn droopescan CMSmap ac-drupal /opt/pentest_tools/Webscanner/Drupal || sudo rm -rf drupwn droopescan CMSmap ac-drupal
 	fi
-	if [[ $(ls /opt/pentest_tools/{"Typo3Scan","T3Scan"} ]]; then
+	if [[ $(ls /opt/pentest_tools/{"Typo3Scan","T3Scan"}) ]]; then
 		sudo mkdir -p /opt/pentest_tools/Webscanner/Typo3 ; mv Typo3Scan T3Scan /opt/pentest_tools/Webscanner/Typo3 || sudo rm -rf Typo3Scan T3Scan
 	fi
-	if [[ $(ls /opt/pentest_tools/{"wpscan","wphunter","WPSeku","Wordpresscan"} ]]; then
+	if [[ $(ls /opt/pentest_tools/{"wpscan","wphunter","WPSeku","Wordpresscan"}) ]]; then
 		sudo mkdir -p /opt/pentest_tools/Webscanner/Wordpress
 		mv wpscan wphunter Wordpresscan WPSeku /opt/pentest_tools/Webscanner/Wordpress || sudo rm -rf wpscan wphunter Wordpresscan WPSeku
 	fi
-	if [[ $(ls /opt/pentest_tools/{"joomscan","joomlavs"} ]]; then
+	if [[ $(ls /opt/pentest_tools/{"joomscan","joomlavs"}) ]]; then
 		sudo mkdir -p /opt/pentest_tools/Webscanner/Joomla ; mv joomscan joomlavs /opt/pentest_tools/Webscanner/Joomla || sudo rm -rf joomscan joomlavs
 	fi
-	if [[ $(ls /opt/pentest_tools/{"moodlescan","mooscan","badmoodle"} ]]; then
+	if [[ $(ls /opt/pentest_tools/{"moodlescan","mooscan","badmoodle"}) ]]; then
 		sudo mkdir -p /opt/pentest_tools/Webscanner/Moodle
 		mv moodlescan badmoodle mooscan /opt/pentest_tools/Webscanner/Moodle || sudo rm -rf moodlescan badmoodle mooscan
 	fi
-	if [[ $(ls /opt/pentest_tools/{"chisel","mitmproxy","mit_mrelay","proxychains-ng"} ]]; then
+	if [[ $(ls /opt/pentest_tools/{"chisel","mitmproxy","mit_mrelay","proxychains-ng"}) ]]; then
 		sudo mkdir -p /opt/pentest_tools/Proxy
 		mv chisel mitmproxy mitm_relay proxychains-ng /opt/pentest_tools/Proxy || sudo rm -rf chisel mitmproxy mitm_relay proxychains-ng
 	fi
-	if [[ $(ls /opt/pentest_tools/{"SIPTools","sipvicious","viproy-voipkit"} ]]; then
+	if [[ $(ls /opt/pentest_tools/{"SIPTools","sipvicious","viproy-voipkit"}) ]]; then
 		sudo mkdir -p /opt/pentest_tools/SIP ; mv viproy-voipkit sipvicious SIPTools /opt/pentest_tools/SIP || sudo rm -rf viproy-voipkit sipvicious SIPTools
 	fi
-	if [[ $(ls /opt/pentest_tools/{"ffuf","wfuzz"} ]]; then
+	if [[ $(ls /opt/pentest_tools/{"ffuf","wfuzz"}) ]]; then
 		sudo mkdir -p /opt/pentest_tools/Fuzzer ; mv ffuf wfuzz /opt/pentest_tools/Fuzzer || sudo rm -rf ffuf wfuzz
 	fi
 	if [[ -d "/opt/pentest_tools/plown" ]]; then
