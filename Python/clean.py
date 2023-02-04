82bc4f53e15429de697b648fb87794a6c8cf484c
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-02-04 10:05:43 +0100

    Update clean.py

--- Python/clean.py
+++ Python/clean.py
@@ -8,7 +8,7 @@ from logger import *
 from sys import argv
 
 # Arrays
-Array_Filter = ['.sh','.deb','.tar.gz','.bz2','.zip','.rar','.xpi']
+Array_Filter = ['.sh','.deb','.tar.gz','.bz2','.zip','.rar','.xpi','.msi']
 
 # Main
 if __name__ == '__main__':
