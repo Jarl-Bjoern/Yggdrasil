a1e0edea283b394338709932d74a266cda8dab94
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-02-05 15:09:13 +0100

    Update configurator.sh

--- Resources/Workfiles/configurator.sh
+++ Resources/Workfiles/configurator.sh
@@ -303,9 +303,9 @@ function File_Installer() {
                                                 for CHECK_GIT in "${Array_Filter_Git[@]}"; do
                                                         if [[ $CHECK_GIT =~ $(echo "$line" | cut -d "/" -f5) ]]; then
                                                                 if [[ "$CHECK_GIT" =~ "/opt/pentest_tools" ]]; then
-                                                                        TEMP_GIT_PATH="$(echo "$CHECK_GIT" | sed "s#/opt/pentest_tools#$OPT_Path#g")"
+                                                                        TEMP_GIT_PATH="${CHECK_GIT/"/opt/pentest_tools"/"$OPT_Path"}"
                                                                 elif [[ "$CHECK_GIT" =~ "/opt/forensic_tools" ]]; then
-                                                                        TEMP_GIT_PATH="$(echo "$CHECK_GIT" | sed "s#/opt/forensic_tools#$OPT_Path#g")"
+                                                                        TEMP_GIT_PATH="${CHECK_GIT/"/opt/forensic_tools"/"$OPT_Path"}"
                                                                 else
                                                                         TEMP_GIT_PATH="$CHECK_GIT"
                                                                 fi
@@ -354,9 +354,9 @@ function File_Installer() {
                                         for CHECK_FILE in "${Array_Filter_Download[@]}"; do
                                                 if [[ $CHECK_FILE =~ $FILE_NAME ]]; then
 							if [[ "$CHECK_FILE" =~ "/opt/pentest_tools" ]]; then
-								TEMP_WGET_PATH="$(echo "$CHECK_FILE" | sed "s#/opt/pentest_tools#$OPT_Path#g")"
+								TEMP_WGET_PATH="${CHECK_FILE/"/opt/pentest_tools"/"$OPT_Path"}"
 							elif [[ "$CHECK_FILE" =~ "/opt/forensic_tools" ]]; then
-								TEMP_WGET_PATH="$(echo "$CHECK_FILE" | sed "s#/opt/forensic_tools#$OPT_Path#g")"
+								TEMP_WGET_PATH="${CHECK_FILE/"/opt/forensic_tools"/"$OPT_Path"}"
 							else
 								TEMP_WGET_PATH="$CHECK_FILE"
 							fi
