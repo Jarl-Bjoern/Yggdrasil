0892d1c6cce309af40f762b6a53493d87493bee2
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-02-06 18:21:35 +0100

    Update and rename Resources/Python/logger.py to Resources/Python/Standard_Operations/Logger.py

--- Resources/Python/Standard_Operations/Logger.py
+++ Resources/Python/Standard_Operations/Logger.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # Rainer Christian Bjoern Herold
 
-# Function
+# Functions
 def Write_Log(Path_Log, Text):
     with open(Path_Log, 'a') as f:
         f.write(f'{Text}\n')
