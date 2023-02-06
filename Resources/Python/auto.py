471390ce7ce5f3ac965f8eaf47dc2565c379556b
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-02-06 18:58:50 +0100

    Update auto.py

--- Resources/Python/auto.py
+++ Resources/Python/auto.py
@@ -4,7 +4,8 @@
 # Version 0.1 29.10.2022
 
 # Libraries
-from os import system
+from os import listdir, system
+from os.path import join
 from pyautogui import hold, hotkey, keyDown, press, write as autowrite
 from sys import argv
 from time import sleep
@@ -17,13 +18,14 @@ def Press_Key(key, seconds): press(key), sleep(seconds)
 
 def Firefox_Addons(Path, License_Parameter):
     Press_Key('win', 2), autowrite('firefox'), sleep(2), Press_Key('return', 5)
-    Press_Hotkey('ctrl', 'l', 2)
-    autowrite('file:'), Press_Hotkey('shift','7', 0.15)
-    for _ in Path.split('/'):
-        Press_Hotkey('shift','7', 0.5), autowrite(_)
-    Press_Key('return', 2.5)
-    if (License_Parameter == False): input('The script was stopped because the parameter "-aL | --accept-licenses" is set to False by default for legal reasons. Please confirm the operation with the "Return" button to continue the program.')
-    else: pass
+    for Extension_File in listdir(Path):
+        Press_Hotkey('ctrl', 'l', 2)
+        autowrite('file:'), Press_Hotkey('shift','7', 0.15)
+        for _ in join(Path, Extension_File).split('/'):
+            Press_Hotkey('shift','7', 0.5), autowrite(_)
+        Press_Key('return', 2.5)
+        if (License_Parameter == False): input('The script was stopped because the parameter "-aL | --accept-licenses" is set to False by default for legal reasons. Please confirm the operation with the "Return" button to continue the program.')
+        else: pass
 
 def SoapUI_Install(Path):
     def Auto_Install():
