ff41124733e0a928bbc9c4c0459ad042c89e44dc
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-02-10 20:10:27 +0100

    Update display-driver.sh

--- Custom/display-driver.sh
+++ Custom/display-driver.sh
@@ -1,14 +1,14 @@
 sudo mkdir -p /opt/drivers ; cd /opt/drivers || return 0
 sudo git clone https://github.com/AdnanHodzic/displaylink-debian
 cd displaylink-debian || return 0
-cat <<EOF | sudo bash displaylink-debian.sh
-i
-Y
-Y
-Y
-Y
-Y
-Y
-Y
-n
-EOF
+#cat <<EOF | sudo bash displaylink-debian.sh
+#i
+#Y
+#Y
+#Y
+#Y
+#Y
+#Y
+#Y
+#n
+#EOF
