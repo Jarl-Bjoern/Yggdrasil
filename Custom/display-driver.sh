e781e5afbdad742d9ced0dd5cfb2ab6a1d141841
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-02-11 10:06:31 +0100

    Update display-driver.sh

--- Custom/display-driver.sh
+++ Custom/display-driver.sh
@@ -3,7 +3,7 @@
 sudo mkdir -p /opt/drivers ; cd /opt/drivers || return 0
 sudo git clone https://github.com/AdnanHodzic/displaylink-debian
 cd displaylink-debian || return 0
-sudo DEBIAN_FRONTEND=noninteractive apt install -y unzip linux-headers-$(uname -r) dkms lsb-release linux-source x11-xserver-utils wget libdrm-dev libelf-dev git pciutils
+sudo DEBIAN_FRONTEND=noninteractive apt install -y unzip linux-headers-"$(uname -r)" dkms lsb-release linux-source x11-xserver-utils wget libdrm-dev libelf-dev git pciutils
 cat <<EOF | sudo bash displaylink-debian.sh
 i
 Y
