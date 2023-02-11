e355ce5aa916b17e1401908e0088ec3b8c4b12b3
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-02-11 11:00:13 +0100

    Update configurator.sh

--- Resources/Workfiles/configurator.sh
+++ Resources/Workfiles/configurator.sh
@@ -751,7 +751,7 @@ else
 fi
 
 # Path_Filtering
-for i in $(find /home -maxdepth 1 | grep -v -E "/home|lost+found") "/root"; do
+for i in $(find /home -maxdepth 1 ! -path "/home" | grep -v "lost+found") "/root"; do
         PATH_BSH="$i/.bashrc"
         PATH_SCREEN="$i/.screenrc"
         PATH_ALIAS="$i/.bash_aliases"
