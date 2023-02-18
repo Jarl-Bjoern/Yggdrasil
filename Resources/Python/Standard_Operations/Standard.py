aa5b236f12f42eda7b3390bf4bd4f36e8203e1e8
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-02-18 19:32:16 +0100

    Update Standard.py

--- Resources/Python/Standard_Operations/Standard.py
+++ Resources/Python/Standard_Operations/Standard.py
@@ -36,16 +36,10 @@ class Standard:
                         with open(join(root, file), 'w') as f:
                             f.write(Temp_Text)
 
-    def Check_dosunix():
-        if ('Installed: (none)' in getoutput(['sudo apt-cache policy dos2unix']) or 'Installiert: (keine)' in getoutput(['sudo apt-cache policy dos2unix'])):
-            print ("Installing dos2unix"), run(['sudo','apt','install','-y','dos2unix'], stdin=DEVNULL, stdout=DEVNULL, stderr=DEVNULL), print ("The installing process was successful.")
-
     def Check_Permissions(File_Path):
         def Permission_Change(File): run(['sudo','chmod','+x',File], stdin=DEVNULL, stdout=DEVNULL, stderr=DEVNULL)
-        #def Converter(File): run(['sudo','dos2unix',File], stdin=DEVNULL, stdout=DEVNULL, stderr=DEVNULL)
 
         for root, _, files in walk(File_Path, topdown=False):
             for file in files:
                 if (file.endswith('.py')): Permission_Change(join(root, file))
                 elif (file.endswith('.sh')): Permission_Change(join(root, file))
-                #Converter(join(root, file))
