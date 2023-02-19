1265b6b44dc1f481b0139657ebebbb1987cdf04d
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-02-19 14:44:37 +0100

    Update Libraries.py

--- Resources/Python/Standard_Operations/Libraries.py
+++ Resources/Python/Standard_Operations/Libraries.py
@@ -5,7 +5,7 @@ try:
     from argparse import ArgumentParser, FileType, RawTextHelpFormatter, SUPPRESS
     from os import makedirs, name as osname, system, walk
     from os.path import dirname, join, realpath
-    from subprocess import DEVNULL, getoutput, run
+    from subprocess import DEVNULL, run
     from sys import stdout
     from time import sleep
 except ModuleNotFoundError as e: input(f"The module was not found\n\n{e}\n\nPlease confirm with the button 'Return'"), exit()
