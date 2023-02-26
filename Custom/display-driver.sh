7d6cb4fa70316d8ce6b779411a0bde112d7fe36c
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-02-26 12:15:01 +0100

    Update display-driver.sh

--- Custom/display-driver.sh
+++ Custom/display-driver.sh
@@ -4,7 +4,8 @@ sudo mkdir -p /opt/drivers ; cd /opt/drivers || return 0
 sudo git clone https://github.com/AdnanHodzic/displaylink-debian
 cd displaylink-debian || return 0
 sudo DEBIAN_FRONTEND=noninteractive apt install -y unzip linux-headers-"$(uname -r)" dkms lsb-release linux-source x11-xserver-utils wget libdrm-dev libelf-dev git pciutils
-cat <<EOF | sudo bash displaylink-debian.sh
+cat <<EOF | sudo bash displaylink-debian.sh || echo -e "\033[0;31mERROR: \033[0m\n\nUnfortunately, the script does not support the cat EOF automatism, please perform the installation manually." && sudo bash displaylink-debian.sh
 i
 Y
-nEOF
+n
+EOF
