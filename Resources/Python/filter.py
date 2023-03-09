1412565a2c31fdc602df73a9889e172ca027e534
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-03-09 20:20:28 +0100

    Update filter.py

--- Resources/Python/filter.py
+++ Resources/Python/filter.py
@@ -46,16 +46,16 @@ alias yggdrasil-education='sudo python3 {yggdrasil_path}/Resources/Python/browse
 alias yggdrasil-hardening='sudo python3 {yggdrasil_path}/Resources/Python/browse.py "{yggdrasil_path}/Information/Hardening.txt"'
 . "$HOME/.cargo/env"
 """
-        Config_Alias_BSH = """alias la='ls -lha --color=auto'
+        Config_Alias_BSH = r"""alias la='ls -lha --color=auto'
 alias grep='grep --color=auto'
 alias df='df -h'
 alias du='du -h'
 alias ffs='sudo $(history -p !!)'
 alias rot13='tr "a-zA-Z" "n-za-mN-ZA-M"'
-function Yggdrasil_File_Reader() {\n    input=$1\n    while IFS= read -r line\n        do\n            if [[ $line =~ "##" ]]; then\n                echo -e "\033[0;32m$line\033[0m"\n            else\n                echo -e "\033[1;33m$line\033[0m"\n            fi\n        done < "$input"; }
+"""+"""function Yggdrasil_File_Reader() {\n    input=$1\n    while IFS= read -r line\n        do\n            if [[ $line =~ "##" ]]; then\n                echo -e "\033[0;32m$line\033[0m"\n            else\n                echo -e "\033[1;33m$line\033[0m"\n            fi\n        done < "$input"; }
 function b64() { echo $1 | base64 -d | xxd; }
 function yggdrasil-vnc() {\n    if [[ $(netstat -tnap | grep 'x11vnc' | awk '{print $7}' | cut -d '/' -f1 | sort -u) ]]; then\n        for i in $(netstat -tnap | grep 'x11vnc' | awk '{print $7}' | cut -d '/' -f1 | sort -u);\n            do kill $i\n        done\n    fi\n    if [[ $(netstat -tnap | grep -v 'tcp6' | awk '{print $4}' | cut -d ':' -f2 | grep '8081') ]]; then\n        kill $(netstat -tnap | grep -v 'tcp6' | grep '0.0.0.0:8081' | awk '{print $7}' | cut -d '/' -f1)\n    fi\n    sudo x11vnc -storepasswd\n    sudo x11vnc -display :0 -autoport -bg -localhost -rfbauth ~/.vnc/passwd -xkb -ncache -ncache_cr -quiet &\n    /usr/share/novnc/utils/novnc_proxy --listen 8081 --vnc localhost:5900 --idle-timeout 900 --ssl-only --key /opt/ssl/pentest-key.pem --cert /opt/ssl/pentest-cert.pem; }
-alias nmap='nmap --exclude $(ip a | grep inet | cut -d " " -f6 | cut -d "/" -f1 | tr "\n" "," | rev | cut -c2- | rev)'
+"""+r"""alias nmap='nmap --exclude $(ip a | grep inet | cut -d " " -f6 | cut -d "/" -f1 | tr "\n" "," | rev | cut -c2- | rev)'
 alias microcode-update='sudo sed -i "s#kali-last-snapshot#kali-rolling#g" /etc/apt/sources.list ; sudo apt clean all ; sudo apt update -y ; sudo apt install -y intel-microcode amd64-microcode ; sudo apt clean all ; sudo sed -i "s#kali-rolling#kali-last-snapshot#g" /etc/apt/sources.list'
 """+rf"""alias git-tools-update='BACK="$(pwd)" ; for i in $(cat {opt_path}/update.info); do echo -e "\033[1;33mUpdate:\033[0m" $i ; cd "$i" ; git pull ; echo -e "\033[0;36m------------------------------------------------\033[0m"; done; cd "$BACK"'
 alias cargo-tools-update='for i in $(cat {opt_path}/update_cargo.info); do echo -e "\033[1;33mUpdate:\033[0m" $i ; cargo install --force $i ; echo -e "\033[0;36m------------------------------------------------\033[0m"; done'
