e9c1ec90199b9ab53f9ee5b5c39fd734e78e39f3
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-05-02 21:51:50 +0200

    Update Colors.py

--- Resources/Python/Standard_Operations/Colors.py
+++ Resources/Python/Standard_Operations/Colors.py
@@ -4,10 +4,10 @@
 
 # Classes
 class Colors:
-    CYAN = '\033[36m'
-    GREEN = '\033[32m'
-    ORANGE = '\033[33m'
-    BLUE = '\033[34m'
-    RED = '\033[31m'
+    CYAN      = '\033[36m'
+    GREEN     = '\033[32m'
+    ORANGE    = '\033[33m'
+    BLUE      = '\033[34m'
+    RED       = '\033[31m'
     UNDERLINE = '\033[4m'
-    RESET = '\033[0m'
+    RESET     = '\033[0m'
