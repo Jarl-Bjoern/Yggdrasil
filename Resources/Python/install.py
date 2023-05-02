5b64a9cdab4e975613921336cc7a5b38d8ea3c97
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-05-02 21:48:20 +0200

    Update install.py

--- Resources/Python/install.py
+++ Resources/Python/install.py
@@ -4,15 +4,18 @@
 # Version 0.1 27.10.2022
 
 # Libraries
-from os import system, walk
-from os.path import dirname, exists, join, realpath
+from os                         import system, walk
+from os.path                    import dirname, exists, join, realpath
 from Standard_Operations.Colors import Colors
 from Standard_Operations.Logger import Write_Log
-from sys import argv
+from sys                        import argv
 
 # Main
 if __name__ == '__main__':
     for root, _, files in walk(str(argv[1]), topdown=False):
         for file in files:
-            if (file.endswith('.deb')): system(f'sudo dpkg -i {join(root, file)}')
-            elif (file.endswith('.sh') or file.endswith('.bash')): system(f'sudo bash {join(root, file)}')
+            if (file.endswith('.deb')):
+                        system(f'sudo dpkg -i {join(root, file)}')
+            elif (file.endswith('.sh') or
+                  file.endswith('.bash')):
+                        system(f'sudo bash {join(root, file)}')
