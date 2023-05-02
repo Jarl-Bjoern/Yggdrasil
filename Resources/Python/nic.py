39c5d0ba08e70a0692a05a3da195565ad25c6b78
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-05-02 21:48:50 +0200

    Update nic.py

--- Resources/Python/nic.py
+++ Resources/Python/nic.py
@@ -14,7 +14,7 @@ for Network_Adapter in range(0, len(Array_NIC)):
                 if (NIC[IP_Address] != "" and NIC[IP_Address] != "UP"):
                         if ("/" in NIC[IP_Address]):
                                 if (NIC[IP_Address].count(':') > 1): print (f'  - {NIC[IP_Address].split("/")[0]} '+Colors.CYAN+'(IPv6)'+Colors.RESET)
-                                else:                                print(f'  - {NIC[IP_Address].split("/")[0]} '+Colors.GREEN+'(IPv4)'+Colors.RESET)
+                                else:                                print (f'  - {NIC[IP_Address].split("/")[0]} '+Colors.GREEN+'(IPv4)'+Colors.RESET)
                         else:
                                 print (Colors.ORANGE+f'{NIC[IP_Address]}:'+Colors.RESET)
         if (Network_Adapter != len(Array_NIC)-1): print(Colors.CYAN+'-----------------------------------------------------------------'+Colors.RESET)
