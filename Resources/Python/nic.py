442ba1fa04121d6246b5ceff5bce4c711bd09d9f
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-05-02 21:48:42 +0200

    Update nic.py

--- Resources/Python/nic.py
+++ Resources/Python/nic.py
@@ -3,7 +3,7 @@
 # Rainer Christian Bjoern Herold
 
 # Libraries
-from subprocess import getoutput
+from subprocess                 import getoutput
 from Standard_Operations.Colors import Colors
 
 Array_NIC = getoutput('ip --brief a | grep "UP" | grep -v -E "lo|docker|veth"').splitlines()
@@ -14,7 +14,7 @@ for Network_Adapter in range(0, len(Array_NIC)):
                 if (NIC[IP_Address] != "" and NIC[IP_Address] != "UP"):
                         if ("/" in NIC[IP_Address]):
                                 if (NIC[IP_Address].count(':') > 1): print (f'  - {NIC[IP_Address].split("/")[0]} '+Colors.CYAN+'(IPv6)'+Colors.RESET)
-                                else: print(f'  - {NIC[IP_Address].split("/")[0]} '+Colors.GREEN+'(IPv4)'+Colors.RESET)
+                                else:                                print(f'  - {NIC[IP_Address].split("/")[0]} '+Colors.GREEN+'(IPv4)'+Colors.RESET)
                         else:
                                 print (Colors.ORANGE+f'{NIC[IP_Address]}:'+Colors.RESET)
         if (Network_Adapter != len(Array_NIC)-1): print(Colors.CYAN+'-----------------------------------------------------------------'+Colors.RESET)
