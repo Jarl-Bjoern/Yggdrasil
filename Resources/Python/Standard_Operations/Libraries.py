5656856e9914214c715ec0126097cc80935bf157
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-05-02 21:50:46 +0200

    Update Libraries.py

--- Resources/Python/Standard_Operations/Libraries.py
+++ Resources/Python/Standard_Operations/Libraries.py
@@ -2,10 +2,10 @@
 # -*- coding: utf-8 -*-
 # Rainer Christian Bjoern Herold
 try:
-    from argparse import ArgumentParser, FileType, RawTextHelpFormatter, SUPPRESS
-    from os import makedirs, name as osname, system, walk
-    from os.path import dirname, join, realpath
+    from argparse   import ArgumentParser, FileType, RawTextHelpFormatter, SUPPRESS
+    from os         import makedirs, name as osname, system, walk
+    from os.path    import dirname, join, realpath
     from subprocess import DEVNULL, run
-    from sys import stdout
-    from time import sleep
+    from sys        import stdout
+    from time       import sleep
 except ModuleNotFoundError as e: input(f"The module was not found\n\n{e}\n\nPlease confirm with the button 'Return'"), exit()
