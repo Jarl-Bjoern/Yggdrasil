556a4b15d50b0e49a0a2bccc42d833654e27edbb
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-07-20 10:36:55 +0200

    Update clean.py

--- Resources/Python/clean.py
+++ Resources/Python/clean.py
@@ -14,10 +14,12 @@ Array_Filter = ['.sh','.deb','.tar.gz','.bz2','.zip','.rar','.xpi','.msi']
 
 # Main
 if __name__ == '__main__':
-    for File in listdir(str(argv[1])):
-        for Filter in Array_Filter:
-            try:
-                if (File.endswith(Filter) or Filter in File):
-                    remove(join(argv[1], File))
-                    Write_Log(dirname(realpath(__file__)).replace('Resources/Python','yggdrasil.log'), Colors.CYAN+"-------------------------------------------------------------------------------\n\n"+Colors.RESET+"The File "+Colors.ORANGE+f"{File}"+Colors.RESET+" was successfully removed.")
-            except FileNotFoundError: pass
+    try:
+        for File in listdir(str(argv[1])):
+            for Filter in Array_Filter:
+                try:
+                    if (File.endswith(Filter) or Filter in File):
+                        remove(join(argv[1], File))
+                        Write_Log(dirname(realpath(__file__)).replace('Resources/Python','yggdrasil.log'), Colors.CYAN+"-------------------------------------------------------------------------------\n\n"+Colors.RESET+"The File "+Colors.ORANGE+f"{File}"+Colors.RESET+" was successfully removed.")
+                except FileNotFoundError: pass
+    except FileNotFoundError: pass
