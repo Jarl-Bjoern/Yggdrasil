dbdf4edea934d599509ecda6180170552bf5661b
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-07-23 09:21:53 +0200

    Update auto.py

--- Resources/Python/auto.py
+++ Resources/Python/auto.py
@@ -58,10 +58,6 @@ def Firefox_Addons(Path, License_Parameter, Button_Path = dirname(realpath(__fil
     except KeyboardInterrupt: print("The program will be closed.")
     finally: kill(Process_ID("firefox"), SIGKILL), sleep(2), hotkey('ctrl','win','up'), sleep(1), hotkey('ctrl','win','up')
 
-def SoapUI_Install(Path):
-    def Auto_Install():
-        pass
-
 def Pycharm_Install(Path):
     def Auto_Install():
         pass
