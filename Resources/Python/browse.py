d802c47e1ad675fa55ecbd5dd47e45f07185527d
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-09-04 19:20:57 +0200

    Update browse.py

--- Resources/Python/browse.py
+++ Resources/Python/browse.py
@@ -4,14 +4,17 @@
 # Vers 0.1 08.11.2022
 
 # Libraries
-from sys import argv
-from time import sleep
+from contextlib import redirect_stdout
+from sys        import argv
+from time       import sleep
 from webbrowser import open as webbrowser_open
 
 # Main
 if __name__ == '__main__':
   with open(argv[1], 'r') as f:
       for URL in f.read().splitlines():
-          try: webbrowser_open(URL)
+          try:
+              with redirect_stdout(None):
+                  webbrowser_open(URL)
           except: pass
           sleep(0.65)
