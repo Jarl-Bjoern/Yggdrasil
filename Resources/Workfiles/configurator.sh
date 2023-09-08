a198ceb0b1d471adc305bf2c17f471c4f1a8bbcb
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-09-08 17:36:13 +0200

    Update configurator.sh

--- Resources/Workfiles/configurator.sh
+++ Resources/Workfiles/configurator.sh
@@ -465,6 +465,12 @@ function File_Installer() {
                                                         chmod +x "$FILE_NAME" ; cd "$2" || return 0
                                                 elif [ "$MODE" = "Archive" ]; then
                                                         wget --content-disposition "$FILE"
+							TEMP_SECOND_PART_FILE="$FILE_NAME"
+
+       
+       							echo $TEMP_SECOND_PART_FILE ; exit
+
+       
                                                         FILE_NAME=$(curl -L --head -s "$FILE" | grep filename | tail -n1 | cut -d "=" -f2)
                                                         if [[ ${#FILE_NAME} -gt 0 ]]; then
                                                                 sudo python3 "$FULL_PATH/Resources/Python/zip.py" "$FILE_NAME" "$2"
