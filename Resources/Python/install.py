8c4fbc5cac4ff7555e123f84dd363f6f3a0548e0
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-12-16 08:42:44 +0100

    Update install.py

--- Resources/Python/install.py
+++ Resources/Python/install.py
@@ -11,28 +11,25 @@ from Standard_Operations.Colors import Colors
 from Standard_Operations.Logger import Write_Log
 from sys                        import argv
 
+# Functions
+def Check_File(file_name, full_path):
+    if (file_name.endswith('.deb') or
+        '.deb' in file_name[-6:]):
+                system(f'sudo dpkg -i {file_name}')
+    elif (file_name.endswith('.sh')   or
+          file_name.endswith('.bash') or
+          '.sh'   in file_name[-6:]   or
+          '.bash' in file_name[-6:]):
+                system(f'sudo bash {file_name}')    
+
 # Main
 if __name__ == '__main__':
     if ('\r' in argv[1]): Temp = argv[1].split('\r')[0]
     else:                 Temp = argv[1]
 
     if (isfile(Temp) or '.deb' in Temp[-6:]):
-        file = Temp
-        if (file.endswith('.deb') or
-            '.deb' in file[-6:]):
-                    system(f'sudo dpkg -i {file}')
-        elif (file.endswith('.sh')   or
-              file.endswith('.bash') or
-              '.sh'   in file[-6:]   or
-              '.bash' in file[-6:]):
-                    system(f'sudo bash {file}')
+        Check_File(Temp)
     elif (isdir(argv[1])):
         for root, _, files in walk(str(Temp), topdown=False):
             for file in files:
-                if (file.endswith('.deb')    or '.deb' in file[-6:]):
-                            system(f'sudo dpkg -i {join(root, file)}')
-                elif (file.endswith('.sh')   or
-                      file.endswith('.bash') or
-                      '.sh'   in file[-6:]   or
-                      '.bash' in file[-6:]):
-                            system(f'sudo bash {join(root, file)}')
+                Check_File(file, join(root, file))
