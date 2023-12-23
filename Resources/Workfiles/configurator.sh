127300995a066cdb5986ba62cf7f539c3396c9b0
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-12-23 16:28:18 +0100

    Update configurator.sh

--- Resources/Workfiles/configurator.sh
+++ Resources/Workfiles/configurator.sh
@@ -1059,7 +1059,7 @@ if [[ "$Switch_UPDATES" == true ]]; then
         if [[ "$Switch_CRON" == true ]]; then
                 sudo python3 "$FULL_PATH/Resources/Python/filter.py" "/etc/crontab" "$OPT_Path" "normal"
         elif [[ "$Switch_SYSTEMD" == true ]]; then
-                sudo python3 "$FULL_PATH/Resources/Python/filter.py" "/usr/lib/systemd/system" "$OPT_Path"
+                sudo python3 "$FULL_PATH/Resources/Python/filter.py" "/usr/lib/systemd/system" "$OPT_Path" "normal"
                 sudo systemctl enable --now Yggdrasil_Cargo_Updater.timer Yggdrasil_Container_Cleaner.timer Yggdrasil_Container_Updates.timer Yggdrasil_GIT_Updater.timer Yggdrasil_PIP_Updater.timer Yggdrasil_System_Updates.timer &>/dev/null
                 sudo systemctl daemon-reload &>/dev/null
         fi
