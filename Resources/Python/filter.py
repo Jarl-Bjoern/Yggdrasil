0016806f69b69910fe881ecd00b734c958966e94
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-12-23 17:21:20 +0100

    Update filter.py

--- Resources/Python/filter.py
+++ Resources/Python/filter.py
@@ -53,6 +53,7 @@ alias yggdrasil-forensic='sudo python3 {yggdrasil_path}/Resources/Python/browse.
 alias yggdrasil-education='sudo python3 {yggdrasil_path}/Resources/Python/browse.py "{yggdrasil_path}/Information/Pages/Education.txt"'
 alias yggdrasil-hardening='sudo python3 {yggdrasil_path}/Resources/Python/browse.py "{yggdrasil_path}/Information/Pages/Hardening.txt"'
 alias yggdrasil-pentesting='sudo python3 {yggdrasil_path}/Resources/Python/browse.py "{yggdrasil_path}/Information/Pages/Infrastructure.txt"'
+alias yggdrasil-web='sudo python3 {yggdrasil_path}/Resources/Python/browse.py "{yggdrasil_path}/Information/Pages/Web.txt"'
 alias yggdrasil-rust-update='wget https://sh.rustup.rs -O /tmp/rust_install.sh ; sudo chmod +x /tmp/rust_install.sh ; sudo bash /tmp/rust_install.sh -y ; sudo rm -f /tmp/rust_install.sh'
 """
         Config_Alias_BSH = r"""alias la='ls -lha --color=auto'
@@ -76,6 +77,7 @@ alias yggdrasil-forensic='sudo python3 {yggdrasil_path}/Resources/Python/browse.
 alias yggdrasil-education='sudo python3 {yggdrasil_path}/Resources/Python/browse.py "{yggdrasil_path}/Information/Pages/Education.txt"'
 alias yggdrasil-hardening='sudo python3 {yggdrasil_path}/Resources/Python/browse.py "{yggdrasil_path}/Information/Pages/Hardening.txt"'
 alias yggdrasil-pentesting='sudo python3 {yggdrasil_path}/Resources/Python/browse.py "{yggdrasil_path}/Information/Pages/Infrastructure.txt"'
+alias yggdrasil-web='sudo python3 {yggdrasil_path}/Resources/Python/browse.py "{yggdrasil_path}/Information/Pages/Web.txt"'
 alias yggdrasil-rust-update='wget https://sh.rustup.rs -O /tmp/rust_install.sh ; sudo chmod +x /tmp/rust_install.sh ; sudo bash /tmp/rust_install.sh -y ; sudo rm -f /tmp/rust_install.sh'
 """
         Config_Alias_Profile = r'. "$HOME/.cargo/env"'
