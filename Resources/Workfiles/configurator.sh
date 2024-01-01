10ff7721e73bf65e1ad00a378b4eba9503a39ee3
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2024-01-01 11:53:46 +0100

    Update configurator.sh

--- Resources/Workfiles/configurator.sh
+++ Resources/Workfiles/configurator.sh
@@ -301,7 +301,7 @@ function Download_Commander() {
 	function Check_For_Skip_Download() {
 		Switch_Skip_Git_Download=false
 		for Check in ${Array_Filter_Download[@]}; do
-			if [[ "$OPT_Path" =~ "$Check" && $(echo "$Check" | rev | cut -d '/' | rev | tr -d '\r') =~ "$1" ]]; then
+			if [[ "$OPT_Path" =~ "$Check" && $(echo "$Check" | rev | cut -d '/' -f1 | rev | tr -d '\r') =~ "$1" ]]; then
 				Switch_Skip_Git_Download=true
 				break
 			fi
