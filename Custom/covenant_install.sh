3b983e9130f8f62da51b436cc2ac6580e50a8c7b
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2024-01-28 11:26:26 +0100

    Update covenant_install.sh

--- Custom/covenant_install.sh
+++ Custom/covenant_install.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-if [ -d "/opt/pentest_tools/Covenant" ]; then
-  cd /opt/pentest_tools/Covenant/Covenant
-  docker build -t covenant .
-  docker run -it -p 7443:7443 -p 80:80 -p 443:443 --name covenant -v /opt/pentest_tools/Covenant/Covenant/Data:/app/Data covenant
+# Variables
+COVENANT_PATH=""
+
+# Main
+if [ -d "$1" ]; then
+    if [[ ! "$1" =~ "Covenant" ]]; then
+        if [[ $(find "$1" -type d -name "Covenant") ]]; then
+            COVENANT_PATH=$(find "$1" -type d -name "Covenant")
+        else
+            echo -e "Covenant was not found.\n\nPlease try again." ; exit
+        fi
+    fi
+
+    if [[ "$COVENANT_PATH" != "" ]]; then
+        cd "$COVENANT_PATH"
+        docker build -t covenant .
+        docker run -it -d -p 7443:7443 -p 80:80 -p 443:443 --name covenant -v "$COVENANT_PATH"/Data:/app/Data covenant
+    fi
+else
+    echo -e "Covenant was not found.\n\nPlease try again." ; exit
 fi
