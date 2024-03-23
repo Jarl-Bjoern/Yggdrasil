9a7e6ae326480afa87798ae49739a2873122c914
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2024-03-23 20:21:48 +0100

    Update configurator.sh

--- Resources/Workfiles/configurator.sh
+++ Resources/Workfiles/configurator.sh
@@ -249,9 +249,9 @@ function header() {
                 echo -e "${CYAN}|${NOCOLOR}   [${ORANGE}4${NOCOLOR}] ${ORANGE}ssh${NOCOLOR}              :   ssh      hardening                 ${CYAN}|${NOCOLOR}"
                 echo -e "${CYAN}|${NOCOLOR}   [${BLUE}5${NOCOLOR}] ${BLUE}apache${NOCOLOR}           :   apache   hardening                 ${CYAN}|${NOCOLOR}"
                 echo -e "${CYAN}|${NOCOLOR}   [${PURPLE}6${NOCOLOR}] ${PURPLE}nginx${NOCOLOR}            :   nginx    hardening                 ${CYAN}|${NOCOLOR}"
-                echo -e "${CYAN}|${NOCOLOR}   [${RED}7${NOCOLOR}] ${GREEN}smb${NOCOLOR}              :   smb      hardening                 ${CYAN}|${NOCOLOR}"
-                #echo -e "${CYAN}|${NOCOLOR}   [${CYAN}8${NOCOLOR}] ${RED}ftp${NOCOLOR}           :   ftp   hardening                 ${CYAN}|${NOCOLOR}"
-                #echo -e "${CYAN}|${NOCOLOR}   [${GREEN}9${NOCOLOR}] ${CYAN}squid${NOCOLOR}            :   squid    hardening                 ${CYAN}|${NOCOLOR}"
+                echo -e "${CYAN}|${NOCOLOR}   [${RED}7${NOCOLOR}] ${RED}smb${NOCOLOR}              :   smb      hardening                 ${CYAN}|${NOCOLOR}"
+                #echo -e "${CYAN}|${NOCOLOR}   [${CYAN}8${NOCOLOR}] ${CYAN}ftp${NOCOLOR}           :   ftp   hardening                 ${CYAN}|${NOCOLOR}"
+                #echo -e "${CYAN}|${NOCOLOR}   [${GREEN}9${NOCOLOR}] ${GREEN}squid${NOCOLOR}            :   squid    hardening                 ${CYAN}|${NOCOLOR}"
         elif [ "$1" = "settings" ]; then
                 echo -e "${CYAN}|${NOCOLOR}  [${RED}1${NOCOLOR}] ${RED}complete${NOCOLOR}      :   complete configuration                 ${CYAN}|${NOCOLOR}"
                 echo -e "${CYAN}|${NOCOLOR}  [${CYAN}2${NOCOLOR}] ${CYAN}updates${NOCOLOR}       :   automated updates                      ${CYAN}|${NOCOLOR}"
