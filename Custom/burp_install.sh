e40fb7922f2cfe8b9903acea30152bde3fccfed1
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2024-04-04 18:06:28 +0200

    Update burp_install.sh

--- Custom/burp_install.sh
+++ Custom/burp_install.sh
@@ -3,8 +3,15 @@
 if [ $1 ]; then
   # Remove_Burp
   apt remove -y burpsuite ; apt autoremove --purge -y
-  
+
+  # Download_Burp
+  website=$(curl -s "https://portswigger.net/burp/releases")
+  version=$(echo "$html" | grep -P "professional-community-" | cut -d '"' -f2 | cut -d '/' -f4 | sort -ur | head -n1)
+  wget "https://portswigger-cdn.net/burp/releases/download?product=pro&version=$version&type=sh" -O "burpsuite_pro_v$version.sh" --quiet --show-progress
+
   # Install_Burp
+  sudo bash "burpsuite_pro_v$version.sh"
+  
 else
   echo "The path of the license file was missing."
 fi
