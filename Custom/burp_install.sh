a835735bb419c6d86f5960358b15d3113faf8f67
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2024-04-06 09:32:58 +0200

    Update burp_install.sh

--- Custom/burp_install.sh
+++ Custom/burp_install.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
 if [ $1 ]; then
-  # Remove_Burp
-  apt remove -y burpsuite ; apt autoremove --purge -y
-
-  # Download_Burp
-  website=$(curl -s "https://portswigger.net/burp/releases")
-  version=$(echo "$html" | grep -P "professional-community-" | cut -d '"' -f2 | cut -d '/' -f4 | sort -ur | head -n1)
-  wget "https://portswigger-cdn.net/burp/releases/download?product=pro&version=$version&type=sh" -O "burpsuite_pro_v$version.sh" --quiet --show-progress
-
-  # Install_Burp
-  sudo bash "burpsuite_pro_v$version.sh"
+  if [[ -f "$1" ]]; then
+    # Install_xclip
+    apt install -y xclip
   
+    # Remove_Burp
+    apt remove -y burpsuite ; apt autoremove --purge -y
+  
+    # Download_Burp
+    website=$(curl -s "https://portswigger.net/burp/releases")
+    version=$(echo "$html" | grep -P "professional-community-" | cut -d '"' -f2 | cut -d '/' -f4 | sort -ur | head -n1)
+    wget "https://portswigger-cdn.net/burp/releases/download?product=pro&version=$version&type=sh" -O "/tmp/burpsuite_pro_v$version.sh" --quiet --show-progress
+  
+    # Install_Burp
+    sudo bash "/tmp/burpsuite_pro_v$version.sh"
+
+    
+  else
+    echo "The provided argument is not a file."
+  fi
 else
   echo "The path of the license file was missing."
 fi
