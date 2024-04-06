0353fa9415b7f1e5a2d6b80ba704ba4ca5fe5343
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2024-04-06 09:57:07 +0200

    Update burp_install.sh

--- Custom/burp_install.sh
+++ Custom/burp_install.sh
@@ -12,11 +12,21 @@ if [ $1 ]; then
     website=$(curl -s "https://portswigger.net/burp/releases")
     version=$(echo "$website" | grep -P "professional-community-" | cut -d '"' -f2 | cut -d '/' -f4 | sed 's/professional-community-//g' | sort -ur | head -n1 | sed 's/-/./g')
     wget "https://portswigger-cdn.net/burp/releases/download?product=pro&version=$version&type=Linux" -O "/tmp/burpsuite_pro_v$version.sh" --quiet --show-progress
+
+    # Clipboard_Cleaner
+    touch "" > /tmp/clean.txt
+
+    # Read_License
+    xclip "$1"
   
     # Install_Burp
     sudo bash "/tmp/burpsuite_pro_v$version.sh"
 
-    
+    # Paste_License
+    # UNDER CONSTRUCTION
+
+    # Clear_Clipboard
+    xclip /tmp/clean.txt ; rm -f /tmp/clean.txt
   else
     echo "The provided argument is not a file or does not exist."
   fi
