4b48036354d04e5a6da3e30bb814a376a3cdc476
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2024-04-13 08:22:31 +0200

    Update burp_install.sh

--- Custom/burp_install.sh
+++ Custom/burp_install.sh
@@ -4,7 +4,7 @@
 # Variables
 TEMP_PATH=$(readlink -f -- "$0")
 SCRIPT_NAME=$(basename "${BASH_SOURCE[0]}")
-FULL_PATH=${TEMP_PATH::-${#SCRIPT_NAME}-21}
+FULL_PATH=${TEMP_PATH::-${#SCRIPT_NAME}-8}
 
 # Main
 if [ $1 ]; then
