3518959524ad259077cabb18bbcc40ac7e82cca2
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2024-04-13 08:16:08 +0200

    Update burp_install.sh

--- Custom/burp_install.sh
+++ Custom/burp_install.sh
@@ -8,19 +8,19 @@ FULL_PATH=${TEMP_PATH::-${#SCRIPT_NAME}-21}
 
 # Main
 if [ $1 ]; then
-    if [[ -f "$1" ]]; then  
+    if [[ -f "$1" ]]; then
         # Remove_Burp
         sudo apt remove -y burpsuite ; sudo apt autoremove --purge -y
-  
+
         # Download_Burp
         website=$(curl -s "https://portswigger.net/burp/releases")
         version=$(echo "$website" | grep -P "professional-community-" | cut -d '"' -f2 | cut -d '/' -f4 | sed 's/professional-community-//g' | sort -ur | head -n1 | sed 's/-/./g')
         wget "https://portswigger-cdn.net/burp/releases/download?product=pro&version=$version&type=Linux" -O "/tmp/burpsuite_pro_v$version.sh" --quiet --show-progress
-  
+
         # Install_Burp
         sudo bash "/tmp/burpsuite_pro_v$version.sh" &
         sudo python3 "$FULL_PATH/Resources/Python/auto.py" "Burp" "Install"
-    
+
         # Paste_License
         /opt/BurpSuitePro/BurpSuitePro &
         sudo python3 "$FULL_PATH/Resources/Python/auto.py" "Burp" "License" "$1"
