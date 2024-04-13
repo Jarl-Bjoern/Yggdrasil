6f6328eea31535e65717de0e40e336eaaa7ec834
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2024-04-13 08:50:10 +0200

    Update burp_install.sh

--- Custom/burp_install.sh
+++ Custom/burp_install.sh
@@ -33,7 +33,7 @@ if [ $1 ]; then
             website=$(curl -s "https://portswigger.net/burp/releases")
             version=$(echo "$website" | grep -P "professional-community-" | cut -d '"' -f2 | cut -d '/' -f4 | sed 's/professional-community-//g' | sort -ur | head -n1 | sed 's/-/./g')
             wget "https://portswigger-cdn.net/burp/releases/download?product=pro&version=$version&type=Linux" -O "/tmp/burpsuite_pro_v$version.sh" --quiet --show-progress
-    
+
             # Install_Burp
             echo -e "${ORANGE}Starting the installer.\n${CYAN}-------------------------------------\n${NOCOLOR}"
             sudo bash "/tmp/burpsuite_pro_v$version.sh" &
