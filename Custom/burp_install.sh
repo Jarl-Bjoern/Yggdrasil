2dfee9725e52de90916f8f26eedb3695da18c4b7
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2024-04-13 08:34:12 +0200

    Update burp_install.sh

--- Custom/burp_install.sh
+++ Custom/burp_install.sh
@@ -9,28 +9,41 @@ FULL_PATH=${TEMP_PATH::-${#SCRIPT_NAME}-8}
 # Main
 if [ $1 ]; then
     if [[ -f "$1" ]]; then
-        echo "Removing BurpSuite Community."
-        # Remove_Burp
-        sudo apt remove -y burpsuite ; sudo apt autoremove --purge -y
-        echo "BurpSuite Community was removed."
+        if [[ $("apt-cache policy burpsuite | grep 'Installed: '") ]];
+            echo "Skipping the deinstallation process of BurpSuite Community."
+        else
+            echo "Removing BurpSuite Community."
+            # Remove_Burp
+            sudo apt remove -y burpsuite ; sudo apt autoremove --purge -y
+            echo "BurpSuite Community was removed."
+        fi
 
-        # Download_Burp
-        website=$(curl -s "https://portswigger.net/burp/releases")
-        version=$(echo "$website" | grep -P "professional-community-" | cut -d '"' -f2 | cut -d '/' -f4 | sed 's/professional-community-//g' | sort -ur | head -n1 | sed 's/-/./g')
-        wget "https://portswigger-cdn.net/burp/releases/download?product=pro&version=$version&type=Linux" -O "/tmp/burpsuite_pro_v$version.sh" --quiet --show-progress
-
-        # Install_Burp
-        sudo bash "/tmp/burpsuite_pro_v$version.sh" &
-        sleep 10
-        sudo python3 "$FULL_PATH/Resources/Python/auto.py" "Burp" "Install" "TRUE" "EMPTY"
+        if [[ -d "/opt/BurpSuitePro" ]]; then
+            echo "Skipping the download and installation process for BurpSuite Professional."
+        else
+            # Download_Burp
+            echo "Downloading the latest BurpSuite Professional version."
+            website=$(curl -s "https://portswigger.net/burp/releases")
+            version=$(echo "$website" | grep -P "professional-community-" | cut -d '"' -f2 | cut -d '/' -f4 | sed 's/professional-community-//g' | sort -ur | head -n1 | sed 's/-/./g')
+            wget "https://portswigger-cdn.net/burp/releases/download?product=pro&version=$version&type=Linux" -O "/tmp/burpsuite_pro_v$version.sh" --quiet --show-progress
+    
+            # Install_Burp
+            echo "Starting the installer."
+            sudo bash "/tmp/burpsuite_pro_v$version.sh" &
+            sleep 10
+            sudo python3 "$FULL_PATH/Resources/Python/auto.py" "Burp" "Install" "TRUE" "EMPTY"
+        fi
 
         # Paste_License
+        echo "Starting BurpSuite Professional"
         /opt/BurpSuitePro/BurpSuitePro &
         sleep 10
         sudo python3 "$FULL_PATH/Resources/Python/auto.py" "Burp" "License" "TRUE" "$1"
 
-        # Remove_Installer
-        rm -f "/tmp/burpsuite_pro_v$version.sh"
+        if [[ ! -d "/opt/BurpSuitePro" ]]; then
+            # Remove_Installer
+            rm -f "/tmp/burpsuite_pro_v$version.sh"
+        fi
     else
         echo "The provided argument is not a file or does not exist."
     fi
