4455fa3426cad77e378f4af49597e74c107203bc
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2026-05-03 10:47:13 +0200

    Update configurator.sh

--- Resources/Workfiles/configurator.sh
+++ Resources/Workfiles/configurator.sh
@@ -366,7 +366,7 @@ function Download_Commander() {
 		elif [[ $Command =~ "cargo" ]]; then
 			eval "$Command $line" || source "$HOME/.cargo/env" && eval "$Command $line"
         elif [[ $Command =~ "pip3" ]]; then
-		    if [[ ! $(pip3 freeze) | grep "^$line"]] ; then
+		    if [[ ! $(pip3 freeze | grep "^$line") ]] ; then
                 eval "$Command $line"
 			else
 			    echo -e "${RED}$line${NOCOLOR} is already installed." | tee -a "$FULL_PATH/yggdrasil.log"
